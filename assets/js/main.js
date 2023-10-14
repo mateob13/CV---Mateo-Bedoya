1c083a701c015a4f904a7d5f90a74350889a6368
Mateo <mbedoya1808@outlook.com>
2023-10-13 22:27:05 -0500

    first commit CV - Mateo Bedoya

---
+++ assets/js/main.js
@@ -0,0 +1,72 @@
+import darkThemeButton from "./dark_theme.js";
+
+const doc = document; // Abreviación de document
+
+doc.addEventListener("DOMContentLoaded", (e) => {
+  // Delegación de eventos
+  scrollTopButton(".scroll-top-btn");
+  printPDFButton(".print-pdf-btn");
+});
+
+darkThemeButton(".dark-theme-btn", ".dark-theme-i");
+
+/* Scroll Top Button */
+
+function scrollTopButton(btn) {
+  const $scrollBtn = doc.querySelector(btn);
+  window.addEventListener("scroll", (e) => {
+    let scrollTop = window.pageYOffset || doc.documentElement.scrollTop;
+
+    if (scrollTop > 150) {
+      $scrollBtn.classList.remove("hidden");
+    } else {
+      $scrollBtn.classList.add("hidden");
+    }
+  });
+  doc.addEventListener("click", (e) => {
+    if (e.target.matches(btn)) {
+      window.scrollTo({
+        behavior: "smooth",
+        top: 0,
+      });
+    }
+  });
+}
+
+/* Scale CV */
+function scaleCV() {
+  doc.body.classList.add("scale-cv");
+}
+
+function removeScale() {
+  doc.body.classList.remove("scale-cv");
+}
+
+/* Generate PDF */
+
+let areaCV = doc.getElementById("area-cv");
+
+let options = {
+  margin: 1,
+  filename: "CV - Mateo Bedoya Betancur.pdf",
+  image: { type: "jpeg", quality: 0.98 },
+  html2canvas: { scale: 4 },
+  jsPDF: { format: "a4", orientation: "portrait" },
+};
+
+/* Print PDF Button */
+
+function printPDFButton(btn) {
+  doc.addEventListener("click", (e) => {
+    if (e.target.matches(btn)) {
+      // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+      scaleCV();
+
+      // 2. The PDF is generated
+      html2pdf(areaCV, options);
+
+      // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+      setTimeout(removeScale, 5000);
+    }
+  });
+}
